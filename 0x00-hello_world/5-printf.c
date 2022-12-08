3ba41ca8c2a75b0615d412616c66a96f2df681a5
AngelBanful <angelbanful@gmail.com>
2022-12-08 15:57:00 -0800

    Task five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 
 /**
  * main - Entry Point
- *
+ * description:programme to print out string with a new line
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("\"with proper grammar, but the outcome is a piece of art,");
